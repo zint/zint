94ece47f082de86200b74fb8cb9e53f9203ddf8f
gitlost <burmartke@gmail.com>
2021-01-28 20:43:26 +0000

    GUI: suppress "Qt WebEngine seems to be initialized from a plugin" warning

--- frontend_qt/main.cpp
+++ frontend_qt/main.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2008 by BogDan Vatra                                    *
- *   bogdan@licentia.eu                                                    *
+ *   Copyright (C) 2008 by BogDan Vatra <bogdan@licentia.eu>               *
+ *   Copyright (C) 2009-2021 by Robin Stuart <rstuart114@gmail.com>        *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -13,17 +13,23 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
+/* vim: set ts=4 sw=4 et : */
 
 #include <QApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-	Q_INIT_RESOURCE(resources);
-	QApplication app(argc, argv);
+    Q_INIT_RESOURCE(resources);
 
-	MainWindow w;
-	w.show();
-	return app.exec();
+#if QT_VERSION >= 0x50400
+    /* Suppresses "Qt WebEngine seems to be initialized from a plugin" warning */
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+#endif
+
+    QApplication app(argc, argv);
+
+    MainWindow w;
+    w.show();
+    return app.exec();
 }
-
