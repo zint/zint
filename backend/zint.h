eabf2ea10e9640a23ae8baa1bc8bc8bea52de49b
gitlost <burmartke@gmail.com>
2021-12-18 17:18:35 +0000

    FAST_MODE: update manual.txt ([b36fba])

--- backend/zint.h
+++ backend/zint.h
@@ -283,6 +283,7 @@ extern "C" {
 #define GS1NOCHECK_MODE         0x0020  /* Do not check validity of GS1 data (except that printable ASCII only) */
 #define HEIGHTPERROW_MODE       0x0040  /* Interpret `height` as per-row rather than as overall height */
 #define FAST_MODE               0x0080  /* Use faster if less optimal encodation for symbologies that support it */
+                                        /* Note: only DATAMATRIX currently */
 
 /* Data Matrix specific options (`symbol->option_3`) */
 #define DM_SQUARE               100     /* Only consider square versions on automatic symbol size selection */
