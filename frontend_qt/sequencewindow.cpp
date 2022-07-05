9870fc6a61c7cb58ea4958578c904b998515ba98
gitlost <burmartke@gmail.com>
2022-07-05 01:35:24 +0100

    GUI: undo Windows * -> + change as formatting data, not filenames, doh

--- frontend_qt/sequencewindow.cpp
+++ frontend_qt/sequencewindow.cpp
@@ -120,11 +120,7 @@ QString SequenceWindow::apply_format(const QString& raw_number)
                     adjusted += raw_number[inpos - 1];
                     inpos--;
                 } else {
-#ifndef _WIN32
                     adjusted += '*';
-#else
-                    adjusted += '+';
-#endif
                 }
                 break;
             default:
